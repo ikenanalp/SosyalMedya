f028e89ac09eafd19800b0027e34211ee2d94b9c
Kenanalp <kenanalp1903@gmail.com>
2026-08-06 15:46:31 +0300

    feedback altyapsı

---
+++ database/migrations/2026_08_06_123330_create_feedbacks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedTinyInteger('type');
+            $table->text('subject');
+            $table->text('message');
+            $table->unsignedTinyInteger('status');
+            $table->text('admin_response')->nullable();
+            $table->unsignedBigInteger('responded_by')->nullable();
+            $table->dateTime('responded_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('responded_by')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedback');
+    }
+};
---
+++ database/migrations/2026_08_06_124314_create_feedback_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedback_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('feedback_id');
+            $table->string('image_url')->nullable();
+            $table->unsignedBigInteger('position')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+
+
+
+            $table->foreign('feedback_id')->references('id')->on('feedbacks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedback_images');
+    }
+};
