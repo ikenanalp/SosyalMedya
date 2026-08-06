3f7007d718fb91c5f2385b7b0b6ae84cea90db6a
Kenanalp <kenanalp1903@gmail.com>
2026-08-06 15:12:48 +0300

    yeni css geçildi, resim ekleme sistemi eklendi, sayfaların resimleri göstermesi sağlandı

--- resources/views/panel/layouts/app.blade.php
+++ resources/views/panel/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> @yield('title') </title>
+
     <!-- Bootstrap 5 CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
@@ -65,6 +66,9 @@
             min-height: 120px;
             box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
         }
+
+        @yield('headcss')
+
     </style>
 </head>
 <body>
